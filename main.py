659455f4eca293c7d511ac1bdcdad9ec2fb245ba
Stavros Stathopoulos <up1101069@ac.upatras.gr>
2026-06-17 13:07:56 +0300

    feat: add the provided test script

---
+++ main.py
@@ -0,0 +1,43 @@
+import robosuite as suite
+from robosuite.controllers import load_composite_controller_config
+
+from src.controllers import SimpleController
+from src.utils import logger
+
+
+controller_config = load_composite_controller_config(controller='BASIC')
+env = suite.make(
+    env_name="NutAssembly",
+    robots="Panda",
+    gripper_types="PandaGripper",
+    controller_configs=controller_config,
+
+    # Vision-based setting
+    has_renderer=True,
+    has_offscreen_renderer=False,
+    use_camera_obs=True,
+    use_object_obs=False,
+
+    # Camera configuration
+    camera_names="agentview",
+    camera_heights=84,
+    camera_widths=84,
+
+    # Task / Control Settings
+    control_freq=20,
+    horizon=500,
+    reward_shaping=True,
+)
+
+controller = SimpleController(env.action_spec)
+obs = env.reset()
+done = False
+episode_reward = 0.0
+
+while not done:
+    action, _ = controller.act(obs)
+    obs, reward, done, info = env.step(action)
+    episode_reward += reward
+
+logger.info(f"Episode reward: {episode_reward}")
+env.close()
