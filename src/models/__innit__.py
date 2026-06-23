95d814acab4856c112c34332fc68df10e113ad5b
Nikos_pap <nikospap603@gmail.com>
2026-06-23 19:40:09 +0300

    Test and Train RL Algorithms

--- src/models/__innit__.py
+++ src/models/__innit__.py
@@ -2,4 +2,5 @@ from .bc_model import BehaviorCloningPolicy
 from .train_bc import train_bc_model
 from .evaluate_bc import evaluate_bc_model
 from .train_rl import train_rl_agent
+from .test_rl import test_rl_agent
 from ..environments.gym_wrapper import RobosuiteGymWrapper
